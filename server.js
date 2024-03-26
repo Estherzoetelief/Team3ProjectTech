31c9c6dd8bb1092c00e473946218a01e27f30586
Mike de Vries <m4ucreatives@gmail.com>
2024-03-26 16:20:42 +0100

    added functionality of multer adding paths and filenames to existing account array as object.

--- server.js
+++ server.js
@@ -120,12 +120,47 @@ app.post('/create-account', (req, res) => {
 
 // })
 
+// app.post('/profile-upload-multiple', upload.array('profile-files', 12), async function (req, res, next) {
+//   try {
+//       console.log('naar database');
+
+//       // Haal de gebruikersnaam op uit de sessie of een andere bron, en sla deze op
+//       const gebruikersnaam = 'VervangDitMetDeGebruikersnaam';
+
+//       // Array om bestandspaden op te slaan
+//       const filepaths = [];
+
+//       // Loop door de geüploade bestanden en haal de bestandsnaam en het pad op
+//       req.files.forEach(file => {
+//           const filepath = file.path;
+//           const filename = file.originalname;
+//           filepaths.push({ filename, filepath });
+//       });
+
+//       // Opslaan van de gegevens in de MongoDB-collectie 'portfolio_uploads'
+//       const result = await imagePathsCollection.insertMany(filepaths.map(filepath => ({
+//           username: gebruikersnaam,
+//           filename: filepath.filename,
+//           filepath: filepath.filepath
+//       })));
+      
+//       console.log(`${result.insertedCount} image paths inserted`);
+
+//       // Stuur een succesvolle reactie terug naar de client
+//       res.status(200).send('File uploaded successfully');
+//   } catch (err) {
+//       console.error('Error uploading files:', err);
+//       res.status(500).send('Error uploading files');
+//   }
+// });
+
+
 app.post('/profile-upload-multiple', upload.array('profile-files', 12), async function (req, res, next) {
   try {
       console.log('naar database');
 
       // Haal de gebruikersnaam op uit de sessie of een andere bron, en sla deze op
-      const gebruikersnaam = 'VervangDitMetDeGebruikersnaam';
+      const gebruikersnaam = 'TestUser';
 
       // Array om bestandspaden op te slaan
       const filepaths = [];
@@ -137,14 +172,25 @@ app.post('/profile-upload-multiple', upload.array('profile-files', 12), async fu
           filepaths.push({ filename, filepath });
       });
 
-      // Opslaan van de gegevens in de MongoDB-collectie 'portfolio_uploads'
-      const result = await imagePathsCollection.insertMany(filepaths.map(filepath => ({
-          username: gebruikersnaam,
-          filename: filepath.filename,
-          filepath: filepath.filepath
-      })));
-      
-      console.log(`${result.insertedCount} image paths inserted`);
+      // Zoek het data-item in de MongoDB-collectie op basis van de gebruikersnaam
+      const existingDataItem = await imagePathsCollection.findOne({ username: gebruikersnaam });
+
+      if (existingDataItem) {
+          // Voeg de nieuwe afbeeldingen toe aan de bestaande array met afbeeldingen
+          const updatedImages = [...existingDataItem.images, ...filepaths];
+
+          // Update het data-item met de nieuwe afbeeldingen
+          await imagePathsCollection.updateOne(
+              { username: gebruikersnaam },
+              { $set: { images: updatedImages } }
+          );
+
+          console.log('Images added to existing data item:', updatedImages);
+      } else {
+          // Als het data-item niet bestaat, maak dan een nieuw item aan met de afbeeldingen
+          await imagePathsCollection.insertOne({ username: gebruikersnaam, images: filepaths });
+          console.log('New data item created with images:', filepaths);
+      }
 
       // Stuur een succesvolle reactie terug naar de client
       res.status(200).send('File uploaded successfully');
